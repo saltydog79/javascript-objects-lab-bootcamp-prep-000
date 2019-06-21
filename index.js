a20fc5d8029f9de11b0fdede37ae5f25f4208a4a
Emily <aspentree79@gmail.com>
2019-06-21 18:19:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ var recipes = {
 function updateObjectWithKeyAndValue(object, key, value) {
   return recipes
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+  return recipes
+}
