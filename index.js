b29a496b42cdd283ba32bb04ca7542a1531711aa
Emily <aspentree79@gmail.com>
2019-06-21 19:00:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   }
 
 function deteleFromObjectByKey(object, key) {
-  delete object[key];
+  delete object.key;
   return object
 }
